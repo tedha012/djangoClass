81d8a4a83766d5e71c8f7a014d8a0b3dda740c6a
tedha012 <teha012@gmail.com>
2022-11-19 13:35:34 +0900

    3-07 model changes

--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -21,6 +21,7 @@ def detail(request, question_id):
     return render(request, "pybo/question_detail.html", context)
 
 
+@login_required(login_url="common:login")
 def answer_create(request, question_id):
 
     """pybo 답변등록"""
@@ -29,21 +30,24 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect("pybo:detail", question_id=question.id)
     else:
-        return HttpResponseNotAllowed("Only POST is possible.")
+        form = AnswerForm()
     context = {"question": question, "form": form}
     return render(request, "pybo/question_detail.html", context)
 
 
+@login_required(login_url="common:login")
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():  # 폼이 유효하다면
             question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
             question.save()  # 데이터를 실제로 저장한다.
             return redirect("pybo:index")
